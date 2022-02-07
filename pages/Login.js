df7e9061a191c2ad5f068db99dccc4c2fe80bd3b
sanjay.b <15eucs117@skcet.ac.in>
2022-02-07 23:13:36 +0530

    implementing Next Auth and Spotify web api

---
+++ pages/Login.js
@@ -0,0 +1,21 @@
+import { getProviders, signIn } from 'next-auth/react'
+
+const Login = () => {
+  return (
+    <div>
+      <img className="mb-5 w-52" src="https://links.papareact.com/9xl" alt="" />{' '}
+    </div>
+  )
+}
+
+export default Login
+
+export async function getServerSideProps() {
+  const providers = await getProviders()
+
+  return {
+    props: {
+      providers,
+    },
+  }
+}
